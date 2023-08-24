3bb95860eafc5b861643894bfeffad55c4117aca
sjana7797 <sjana7797@gmail.com>
2023-08-24 18:14:35 +0530

    link fix

--- components/home/header/index.tsx
+++ components/home/header/index.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import React from "react";
 import { UserDesignation, UserName } from "~/constants/home";
-import { navs, profile, socials } from "../constants";
+import { navs, profile } from "../constants";
 import Nav from "./nav";
 import Socials from "./social";
+import { TypeAnimation } from "react-type-animation";
 
 type Props = {};
 
@@ -17,6 +18,16 @@ function Header({}: Props) {
         <h2 className="mt-3 text-lg font-medium tracking-tight text-teal-400 sm:text-xl">
           {UserDesignation}
         </h2>
+        <TypeAnimation
+          sequence={["Fullstack", 1000, "Frontend", 1000, "Backend", 1000]}
+          wrapper="h3"
+          className="text-3xl font-medium text-cyan-500 sm:text-2xl"
+          cursor={true}
+          repeat={Infinity}
+          speed={20}
+          deletionSpeed={10}
+          preRenderFirstString
+        />
         <p className="mt-4 max-w-xs leading-normal">{profile}</p>
         <nav className="nav hidden lg:block" aria-label="In-page jump links">
           <ul className="mt-16 w-max list-none">
--- components/home/header/nav.tsx
+++ components/home/header/nav.tsx
@@ -8,16 +8,12 @@ type Props = {
 function Nav({ nav }: Props) {
   return (
     <li>
-      <Link
-        shallow
-        href={nav.href}
-        className="active group flex items-center py-3"
-      >
+      <a href={nav.href} className="active group flex items-center py-3">
         <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
         <span className="nav-text text-xs font-bold uppercase  tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
           {nav.name}
         </span>
-      </Link>
+      </a>
     </li>
   );
 }
