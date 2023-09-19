cfc99222cc96ba762a5d9e34641b1aa6db35bf77
Sanjay Jana <sjana7797@gmail.com>
2023-09-19 18:59:49 +0530

    feat: adding skill page (#4)

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -1,13 +1,49 @@
-import Link from "next/link";
+import Card from "~/components/skills/card";
+import { skills } from "~/components/skills/constant";
+import { Button } from "~/components/ui/button";
+import { IoArrowBackOutline } from "react-icons/io5";
+import { useRouter } from "next/router";
+import { motion } from "framer-motion";
 
 type Props = {};
 
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
+      type: "tween",
+    },
+  },
+};
+
 function Skills({}: Props) {
+  const router = useRouter();
   return (
-    <div>
-      Skills
-      <Link href="/">home</Link>
-    </div>
+    <main className="relative p-5">
+      <Button
+        variant={"ghost"}
+        size={"icon"}
+        onClick={() => {
+          router.back();
+        }}
+        className="fixed left-5 top-5"
+      >
+        <IoArrowBackOutline />
+      </Button>
+      <h1 className="text-center text-3xl font-bold uppercase">Skills</h1>
+      <motion.ul
+        className="mx-auto mt-10 grid max-w-2xl grid-cols-3 gap-x-5 gap-y-10 p-5 md:grid-cols-4 md:gap-7 lg:grid-cols-5 lg:gap-8"
+        variants={container}
+        initial="hidden"
+        animate="show"
+      >
+        {skills.map((skill) => {
+          return <Card key={skill.id} skill={skill} />;
+        })}
+      </motion.ul>
+    </main>
   );
 }
 
